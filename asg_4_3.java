4196546170eb4edc081dbfe88252db10f520ba41
Mahee1911 <110080237+Mahee1911@users.noreply.github.com>
2022-09-19 18:07:49 +0530

    Add files via upload

---
+++ asg_4_3.java
@@ -0,0 +1,28 @@
+//21CE029 : MAHEE GADHIYAZ
+//PART : 4
+//PRACTICAL : 3
+import java.util.*;
+class asg_4_3
+{
+public static void main(String ...arg)
+{
+try
+{
+System.out.println("Try block");
+int [] arr= new int[4];
+int i= arr[4];
+} //Exception
+catch(ArithmeticException e) //catch block 2
+{
+System.out.println(e);
+}
+catch(ArrayIndexOutOfBoundsException e) //catch block 2
+{
+System.out.println(e);
+}
+finally //finally block
+{
+System.out.println("This is finally block");
+}
+}
+}
